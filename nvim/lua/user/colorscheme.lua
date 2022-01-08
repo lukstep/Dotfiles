2ce3d2ddbd5300547b23b34406dc8ee9f82645d4
lukstep <lukaszstepien02@gmail.com>
2022-01-08 11:53:59 +0100

    Add colorscheme setup and validation

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,9 @@
+local colorscheme = "deus"
+
+local is_colorschem_avaiable = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not is_colorschem_avaiable then
+    vim.notify("Colorscheme " .. colorscheme .. " not found!")
+    return
+end
+
