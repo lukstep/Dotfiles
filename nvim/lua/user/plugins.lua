1804394c5a42207fe14b6bc439cfbbfef17f7677
lukstep <lukaszstepien02@gmail.com>
2022-01-22 16:12:10 +0100

    Add impatient

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -95,6 +95,7 @@ return packer.startup(function(use)
     use 'nvim-lualine/lualine.nvim'
     use "akinsho/toggleterm.nvim"
     use 'goolord/alpha-nvim'
+    use 'lewis6991/impatient.nvim'
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
