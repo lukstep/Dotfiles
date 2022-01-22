94fbe384a01bb250b2c148f41ca9ad98cfd37676
lukstep <lukaszstepien02@gmail.com>
2022-01-22 13:14:08 +0100

    Add telescope live_grep keymap

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -88,7 +88,7 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- Telescope
 keymap("n", "<c-p>", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-F>", "<cmd>Telescope grep_string<cr>", opts)
--- keymap("n", "<c-F>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<leader>lg", "<cmd>Telescope live_grep<cr>", opts)
 
 -- Null-ls
 keymap("n", "<leader>f", "<cmd>Format<cr>", opts)
