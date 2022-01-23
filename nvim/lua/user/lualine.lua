9e98f942af6acff798a573d71e7d55e4a63d305b
lukstep <lukaszstepien02@gmail.com>
2022-01-23 15:45:03 +0100

    Lualine config clean-up

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -21,7 +21,7 @@ local diff = {
 	"diff",
 	colored = false,
 	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width
+    cond = hide_in_width
 }
 
 local mode = {
@@ -29,12 +29,6 @@ local mode = {
 	fmt = function(str)
 		return "-- " .. str .. " --"
 	end,
-}
-
-local filetype = {
-	"filetype",
-	icons_enabled = false,
-	icon = nil,
 }
 
 local branch = {
@@ -47,16 +41,6 @@ local location = {
 	"location",
 	padding = 0,
 }
-
--- cool function for progress
-local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
-end
 
 local spaces = function()
 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
