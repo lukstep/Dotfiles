57643fba7edbabd6270d52441399e7c0dd7906c0
lukstep <lukaszstepien02@gmail.com>
2022-01-25 19:15:22 +0100

    Fix LSP highlight

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -45,19 +45,23 @@ M.setup = function()
 end
 
 local function lsp_highlight_document(client)
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-      false
-    )
-  end
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+        vim.cmd [[
+            hi LspReferenceRead cterm=underline ctermbg=none gui=underline guibg=none
+            hi LspReferenceText cterm=underline ctermbg=none gui=underline guibg=none
+            hi LspReferenceWrite cterm=underline ctermbg=none gui=underline guibg=none
+            hi link LspReferenceRead Folded
+            hi link LspReferenceText Folded
+            hi link LspReferenceWrite Folded
+                ]]
+        vim.api.nvim_exec([[
+            augroup lsp_document_highlight
+                autocmd! * <buffer>
+                autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+            augroup END]], false)
+    end
 end
 
 local function lsp_keymaps(bufnr)
