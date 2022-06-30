a47d86d0840696dfd2336cdc4c5e07a2a964f484
lukstep <lukaszstepien02@gmail.com>
2022-06-30 18:51:07 +0200

    Add parameters to clangd LS

--- nvim/lua/user/lsp/settings/clangd.lua
+++ nvim/lua/user/lsp/settings/clangd.lua
@@ -1,8 +1,10 @@
 return {
     cmd = {
             "clangd",
+            "--inlay-hints",
             "--background-index",
             "--suggest-missing-includes",
+            "--completion-style=detailed",
             "-j=4",
             "--log=verbose"
         },
